9e343ad146596279e84ff212c0b81721ab975df7
Paul Riley <paul.riley@puppet.com>
2021-03-31 22:17:25 -0600

    putting in the top of the file

--- libs/forgecontroller.py
+++ libs/forgecontroller.py
@@ -1,22 +1,19 @@
 #!/usr/bin/env python3
 
 ###############################
-#
 # node Group controller
-# I'm not a fan of camel_case... but PEP 8 says do it this way
+###############################
 
 import sys, os, json, requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 from types import SimpleNamespace
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-#This is a double check... Thanks Python
 file_dir = os.path.dirname(__file__)
 sys.path.append(file_dir)
 
 class Forgecontroller:
 
     def __init__(self):
-        self.group_obj_list = []
         self.headers = {}
         self.__connection = None
         self.__token = None
@@ -59,34 +56,36 @@ class Forgecontroller:
         resp = requests.get(self.__connection + uri, verify=False, headers=self.headers)
 
         paginate_list = []
-        module_list = []
-
-        #build the list of pages to get based on the modules we want.
-        pages = int(number/100) + (number % 100 > 0)
-        for i in list(range(pages)) :
-            offset = i*100
-            if offset > 0 :
-                #ignore the first offset. We already have that data.
-                paginate_list.append('/v3/releases?hide_deprecated=true&limit=100&offset=' + str(offset))
+        module_list = ["forge 'https://forge.puppet.com'"]
 
         if resp.status_code == 200 :
-            # This worked! Lets start geting some modules.
-            # This turns the json automatically into a model without having to do that heavy lifting :)
-            # Apparently... the .json method creates lists that "look" like json
-            # This does not encode the json into strings correctly. Which is crap.
-            # because the method is called .json() why not call it .to_dictionary()?
-            # if you need to turn it in parseable json: json.loads(data.json()) :(
-            first_ref = json.loads(json.dumps(resp.json()),object_hook=lambda d: SimpleNamespace(**d))
+            # This worked the first time. Let's paginate and get data.
 
-            #first time through. let's save some rest calls
-            for single_mod in first_ref.results :
-                module_list.append("mod '" + single_mod.module.slug + "','" + single_mod.version + "'")
+            #build the list of pages to get based on the modules we want.
+            pages = int(number/100) + (number % 100 > 0)
+            for i in list(range(pages)) :
+                offset = i*100
+                if offset > 0 :
+                    #ignore the first offset. We already have that data.
+                    paginate_list.append('/v3/releases?hide_deprecated=true&limit=100&offset=' + str(offset))
 
-        for next_uri in paginate_list:
-            resp = requests.get(self.__connection + next_uri, verify=False, headers=self.headers)
-            if resp.status_code == 200 :
-                next_ref = json.loads(json.dumps(resp.json()),object_hook=lambda d: SimpleNamespace(**d))
-                for single_mod in next_ref.results :
+
+                # This turns the json automatically into a model without having to do that heavy lifting :)
+                # Apparently... the .json method creates lists that "look" like json
+                # This does not encode the json into strings correctly. Which is crap.
+                # because the method is called .json() why not call it .to_dictionary()?
+                # if you need to turn it in parseable json: json.loads(data.json()) :(
+                first_ref = json.loads(json.dumps(resp.json()),object_hook=lambda d: SimpleNamespace(**d))
+
+                #first time through. let's save some rest calls
+                for single_mod in first_ref.results :
                     module_list.append("mod '" + single_mod.module.slug + "','" + single_mod.version + "'")
+
+            for next_uri in paginate_list:
+                next_resp = requests.get(self.__connection + next_uri, verify=False, headers=self.headers)
+                if next_resp.status_code == 200 :
+                    next_ref = json.loads(json.dumps(resp.json()),object_hook=lambda d: SimpleNamespace(**d))
+                    for single_mod in next_ref.results :
+                        module_list.append("mod '" + single_mod.module.slug + "',    '" + single_mod.version + "'")
 
         return module_list
